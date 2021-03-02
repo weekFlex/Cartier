aca1c5e51bd1067d35158ff84a5f9829db0210ad
MinseungSeon <seonminseung@naver.com>
2021-03-03 03:30:27 +0900

    first commit

---
+++ WeekFlex/WeekFlex/Global/Extension/UILabel+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UILabel+Extension.swift
+//  WeekFlex
+//
+//  Created by 선민승 on 2021/03/03.
+//
+
+import UIKit
+
+// MARK: setLabel function for UILabel
+
+// Extension 설명: 매번 label 내용, 색깔, 폰트 세가지를 세 줄로 작성하는 것을 하나의 function 으로 합쳐주었습니다
+
+// 사용법: MyLabel.setLabel(text: "label내용", color: .black, size: 10)
+
+extension UILabel {
+    func setLabel(text: String, color: UIColor, size: CGFloat){
+        self.text = text
+//        self.font = .spoqaRegular(size: size)
+        self.textColor = color
+    }
+}
---
+++ WeekFlex/WeekFlex/Support/Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekFlex
+//
+//  Created by 선민승 on 2021/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
