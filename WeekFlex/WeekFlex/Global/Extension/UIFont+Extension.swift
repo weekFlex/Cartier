a4da533536a74e884563a70dfb5d3fb22996e83f
xwoud <xwoud@naver.com>
2021-03-04 16:55:47 +0900

    [feat] font 함수 추가
    body: 애플 고딕 관련 함수 추가
    
    Related to: #4

--- WeekFlex/WeekFlex/Global/Extension/UIFont+Extension.swift
+++ WeekFlex/WeekFlex/Global/Extension/UIFont+Extension.swift
@@ -9,4 +9,30 @@ import UIKit
 
 extension UIFont {
     
+    // MARK: AppleSDGothicNeo Font
+    
+    class func appleBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Bold", size: size)!
+    }
+    
+    class func appleThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Thin", size: size)!
+    }
+    
+    class func appleRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Regular", size: size)!
+    }
+    
+    class func appleLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Light", size: size)!
+    }
+    
+    class func appleMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-Medium", size: size)!
+    }
+    
+    class func appleSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "AppleSDGothicNeo-SemiBold", size: size)!
+    }
+    
 }
