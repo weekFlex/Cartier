99735a4295d4b6658642a404715fb635b2825480
dohan <adfilm2@naver.com>
2021-03-07 19:40:16 +0900

    [feat] floating button 애니메이션 추가

--- WeekFlex/WeekFlex/Screen/MainHome/ViewController/MainHomeVC.swift
+++ WeekFlex/WeekFlex/Screen/MainHome/ViewController/MainHomeVC.swift
@@ -13,6 +13,10 @@ class MainHomeVC: UIViewController {
     //MARK: Variable
     
     private var shouldCollaps = false
+    var isFloating = false
+    lazy var floatings: [UIButton] = [self.addTaskBtn, self.getRoutineBtn]
+   
+
     
     //MARK: IBOutlet
     
@@ -20,15 +24,33 @@ class MainHomeVC: UIViewController {
     @IBOutlet weak var expendedHeader: UIView!
     @IBOutlet weak var headerHeight: NSLayoutConstraint!
     @IBOutlet weak var foldingBtn: UIButton!
+    @IBOutlet weak var floatingStackView: UIStackView!
+    @IBOutlet weak var showFloatingBtn: UIButton!
+    @IBOutlet weak var addTaskBtn: UIButton!
+    @IBOutlet weak var getRoutineBtn: UIButton!
+    
+    
     
     //MARK: IBAction
     
-    @IBAction func buttonTap(_ sender: Any) {
-        if shouldCollaps{
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        if shouldCollaps {
             animateView(isCollaps: false,  height: 0)
         }else {
             animateView(isCollaps: true,  height: 104)
         }
+    }
+    @IBAction func floatingBtnDidTap(_ sender: Any) {
+        if isFloating {
+            hideFloating()
+        }else{
+            showFloating()
+        }
+        
+        isFloating = !isFloating
+        UIView.animate(withDuration: 0.3) {
+            showFloatingBtn.transform = roatation
+                }
     }
     
     //MARK: Life Cycle
@@ -51,13 +73,35 @@ extension MainHomeVC {
     var buttonTitle: String {
         return shouldCollaps ? "hide" : "show"
     }
-
+    
     private func animateView(isCollaps:Bool ,  height:Double){
         shouldCollaps = isCollaps
         headerHeight.constant = CGFloat(height)
         foldingBtn.setTitle(buttonTitle, for: .normal)
         UIView.animate(withDuration: 0.3){
             self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func hideFloating(){
+        floatings.reversed().forEach { button in
+            UIView.animate(withDuration: 0.3) {
+                button.isHidden = true
+                self.view.layoutIfNeeded()
+            }
+            
+        }
+    }
+    
+    private func showFloating(){
+        floatings.forEach { [weak self] button in
+            button.isHidden = false
+            button.alpha = 0
+            
+            UIView.animate(withDuration: 0.3) {
+                button.alpha = 1
+                self?.view.layoutIfNeeded()
+            }
         }
     }
 }
---
+++ WeekFlex/WeekFlex/Screen/MainHome/ViewModel/MainHomeVM.swift
@@ -0,0 +1,9 @@
+//
+//  MainHomeVM.swift
+//  WeekFlex
+//
+//  Created by dohan on 2021/03/07.
+//
+
+import Foundation
+
