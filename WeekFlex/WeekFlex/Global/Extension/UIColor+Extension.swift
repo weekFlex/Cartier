a4d80d67c734fe91dd1b4e4ef699fe45619a7002
MinseungSeon <seonminseung@naver.com>
2021-03-19 14:00:04 +0900

    [style] 루틴 추가 뷰 -  테이블뷰, 헤더라벨, 섭타이틀라벨 추가함

---
+++ WeekFlex/WeekFlex/Global/Extension/UIColor+Extension.swift
@@ -0,0 +1,105 @@
+//
+//  UIColor+Extension.swift
+//  WeekFlex
+//
+//  Created by 선민승 on 2021/03/19.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var bgSelected: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray1: UIColor {
+    return UIColor(white: 238.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray2: UIColor {
+    return UIColor(white: 204.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray3: UIColor {
+    return UIColor(red: 164.0 / 255.0, green: 164.0 / 255.0, blue: 169.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray4: UIColor {
+    return UIColor(white: 102.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray5: UIColor {
+    return UIColor(white: 51.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var black: UIColor {
+    return UIColor(white: 24.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color1: UIColor {
+    return UIColor(red: 1.0, green: 104.0 / 255.0, blue: 104.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color5: UIColor {
+    return UIColor(red: 244.0 / 255.0, green: 175.0 / 255.0, blue: 206.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color6: UIColor {
+    return UIColor(red: 104.0 / 255.0, green: 79.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color3: UIColor {
+    return UIColor(red: 1.0, green: 214.0 / 255.0, blue: 67.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color2: UIColor {
+    return UIColor(red: 1.0, green: 157.0 / 255.0, blue: 67.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color7: UIColor {
+    return UIColor(red: 141.0 / 255.0, green: 166.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color8: UIColor {
+    return UIColor(red: 42.0 / 255.0, green: 181.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color4: UIColor {
+    return UIColor(red: 249.0 / 255.0, green: 235.0 / 255.0, blue: 136.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color9: UIColor {
+    return UIColor(red: 0.0, green: 215.0 / 255.0, blue: 121.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color12: UIColor {
+    return UIColor(red: 77.0 / 255.0, green: 81.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color11: UIColor {
+    return UIColor(red: 110.0 / 255.0, green: 201.0 / 255.0, blue: 196.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color10: UIColor {
+    return UIColor(red: 34.0 / 255.0, green: 185.0 / 255.0, blue: 176.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color13: UIColor {
+    return UIColor(red: 143.0 / 255.0, green: 114.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color14: UIColor {
+    return UIColor(red: 1.0, green: 102.0 / 255.0, blue: 65.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var color15: UIColor {
+    return UIColor(red: 251.0 / 255.0, green: 101.0 / 255.0, blue: 209.0 / 255.0, alpha: 1.0)
+  }
+
+}
--- WeekFlex/WeekFlex/Screen/MyRoutine/ViewController/MyRoutineListVC.swift
+++ WeekFlex/WeekFlex/Screen/MyRoutine/ViewController/MyRoutineListVC.swift
@@ -10,14 +10,17 @@ import UIKit
 class MyRoutineListVC: UIViewController {
 
     // MARK: IBOutlet
-    @IBOutlet var backButton: UIButton!
     
+    @IBOutlet var backButton: UIButton!
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var subLabel: UILabel!
+    @IBOutlet var routineTableView: UITableView!
+    @IBOutlet var routineCreateButton: UIView!
     
     // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setLayout()
     }
     
@@ -28,6 +31,9 @@ extension MyRoutineListVC {
     // MARK: function
     
     func setLayout() {
+        backButton.setImage(UIImage(named: "icon32BackBlack"), for: .normal)
+        headerLabel.setLabel(text: "나의 루틴", color: .black, font: .appleBold(size: 24))
+        subLabel.setLabel(text: "루틴 카드로 쉽게 할 일을 추가하세요", color: .gray5, font: .appleRegular(size: 16))
         
     }
 }
