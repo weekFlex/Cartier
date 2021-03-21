beadb23a52242c1208d6f1eda03e5a3e9471950f
MinseungSeon <seonminseung@naver.com>
2021-03-21 17:23:28 +0900

    [upload] metropolis 폰트 디자이너분이 올려주신 파일 Font 폴더 새로 만들어 추가해주었음.

--- WeekFlex/WeekFlex/Screen/MyRoutine/ViewController/MyRoutineListVC.swift
+++ WeekFlex/WeekFlex/Screen/MyRoutine/ViewController/MyRoutineListVC.swift
@@ -15,7 +15,12 @@ class MyRoutineListVC: UIViewController {
     @IBOutlet var headerLabel: UILabel!
     @IBOutlet var subLabel: UILabel!
     @IBOutlet var routineTableView: UITableView!
-    @IBOutlet var routineCreateButton: UIView!
+    
+    // new routine button
+    @IBOutlet var routineCreateButtonView: UIView!
+    @IBOutlet var routinCreateImageView: UIImageView!
+    @IBOutlet var routineCreateLabel: UILabel!
+    
     
     // MARK: Life Cycle
 
@@ -31,9 +36,18 @@ extension MyRoutineListVC {
     // MARK: function
     
     func setLayout() {
+        
+        // 네비게이션 바
+        self.navigationController?.isNavigationBarHidden = true
+        
+        // 헤더
         backButton.setImage(UIImage(named: "icon32BackBlack"), for: .normal)
         headerLabel.setLabel(text: "나의 루틴", color: .black, font: .appleBold(size: 24))
         subLabel.setLabel(text: "루틴 카드로 쉽게 할 일을 추가하세요", color: .gray5, font: .appleRegular(size: 16))
         
+        // new routine button
+        routineCreateButtonView.setBorder(borderColor: .black, borderWidth: 3)
+        routinCreateImageView.image = UIImage(named: "icon24PlusVisual")
+        routineCreateLabel.setLabel(text: "New Routine", color: .black, font: .appleBold(size: 20))
     }
 }
