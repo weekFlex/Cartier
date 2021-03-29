a27ac3df242338612e748966ca0151544b91164e
xwoud <xwoud@naver.com>
2021-03-29 18:05:46 +0900

    [style] 코드 구조 잡기
    body: Mark 문법으로 코드 구조 잡기
    
    Related to: #13

--- WeekFlex/WeekFlex/Screen/AddRoutine/ViewController/MakeRoutineNameVC.swift
+++ WeekFlex/WeekFlex/Screen/AddRoutine/ViewController/MakeRoutineNameVC.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class MakeRoutineNameVC: UIViewController {
 
+    
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var checkButton: UIButton!
@@ -16,22 +21,23 @@ class MakeRoutineNameVC: UIViewController {
     @IBOutlet weak var explainLabel: UILabel!
     @IBOutlet weak var routineNameTextField: UITextField!
     
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+}
+
+// MARK: Extension
+
+extension MakeRoutineNameVC {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: Function
+    
+    
 }
