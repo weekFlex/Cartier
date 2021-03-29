81bb023fcdbce5475a8d186b9f60776cc4010f9b
xwoud <xwoud@naver.com>
2021-03-29 19:05:46 +0900

    [style] 루틴 이름 적기 스타일 완료
    body: textfield에 대한 style 완료
    
    Related to: #13

--- WeekFlex/WeekFlex/Screen/AddRoutine/ViewController/MakeRoutineNameVC.swift
+++ WeekFlex/WeekFlex/Screen/AddRoutine/ViewController/MakeRoutineNameVC.swift
@@ -26,10 +26,25 @@ class MakeRoutineNameVC: UIViewController {
     // MARK: Life Cycle Part
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        setView()
+        setTextField()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // 이 뷰에 들어오자 마자 바로 키보드 띄우고 cursor 포커스 주기
+        self.routineNameTextField.becomeFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 뷰 클릭 시 키보드 내리기
+        view.endEditing(true)
+    }
+
+    
 
 }
 
@@ -39,5 +54,63 @@ extension MakeRoutineNameVC {
     
     // MARK: Function
     
+    func setView() {
+        
+        titleLabel.setLabel(text: "루틴 추가하기", color: .black, font: .appleMedium(size: 18))
+        explainLabel.setLabel(text: "루틴 이름을 입력해주세요", color: .black, font: .appleBold(size: 20))
+        
+        checkButton.isEnabled = false
+    }
+    
+    func setTextField() {
+        
+        routineNameTextField.borderStyle = .none
+        routineNameTextField.placeholder = "ex. English Master"
+        routineNameTextField.font = .metroBold(size: 28)
+        
+        routineNameTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        // routineNameTextField가 수정될 때 마다 실행
+        
+        routineNameTextField.delegate = self
+        
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        
+        if textField.text?.count == 0 || textField.text == nil {
+            // Text가 존재하지 않을 때 버튼 비활성화
+            
+            explainLabel.text = "ex. English Master"
+            checkButton.setImage(UIImage(named: "icon32CheckInactive"), for: .normal)
+            checkButton.isEnabled = false
+        } else {
+            // Text가 존재할 때 버튼 활성화
+            
+            explainLabel.text = "멋진 이름이에요! *-*"
+            checkButton.setImage(UIImage(named: "icon32CheckBlack"), for: .normal)
+            checkButton.isEnabled = true
+        }
+        
+    }
+    
+}
+
+// MARK: UITextFieldDelegate
+
+extension MakeRoutineNameVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 리턴 키 클릭 시
+        
+        textField.endEditing(true)
+        return true
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        // textField 클릭하면 무조건 키보드 올라오게
+        textField.becomeFirstResponder()
+    }
     
 }
