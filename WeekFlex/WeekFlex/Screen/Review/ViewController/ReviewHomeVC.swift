c288c5dd4e5cba1beb31fec479983bd51379b61e
dohan <adfilm2@naver.com>
2021-06-09 21:52:57 +0900

    [feat] 회고 홈 뷰 컬렉션뷰 추가

---
+++ WeekFlex/WeekFlex/Screen/Review/ViewController/ReviewHomeVC.swift
@@ -0,0 +1,22 @@
+//
+//  ReviewVC.swift
+//  WeekFlex
+//
+//  Created by dohan on 2021/06/06.
+//
+
+import Foundation
+import UIKit
+
+class ReviewHomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        
+    }
+    
+    func setLayout(){
+        
+    }
+}
--- WeekFlex/WeekFlex/Screen/Review/ViewController/ReviewVC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ReviewVC.swift
-//  WeekFlex
-//
-//  Created by dohan on 2021/06/06.
-//
-
-import Foundation
