2427793f1bde329ec7f5434933bca6accbe789eb
dohan <adfilm2@naver.com>
2021-06-11 20:55:37 +0900

    [feat] 리뷰목록 CollectionViewCell 레이아웃 설정

---
+++ WeekFlex/WeekFlex/Screen/Review/Cell/ReviewCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewCellCollectionViewCell.swift
+//  WeekFlex
+//
+//  Created by dohan on 2021/06/11.
+//
+
+import UIKit
+
+class ReviewCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
