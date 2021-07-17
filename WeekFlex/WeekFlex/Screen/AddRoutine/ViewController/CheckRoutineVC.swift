fb523072fd30d96de0a1d308b5c18fe30a1c3e35
xwoud <xwoud@naver.com>
2021-07-18 00:29:46 +0900

    [upload] CheckRoutineVC 생성

---
+++ WeekFlex/WeekFlex/Screen/AddRoutine/ViewController/CheckRoutineVC.swift
@@ -0,0 +1,35 @@
+//
+//  CheckRoutineVC.swift
+//  WeekFlex
+//
+//  Created by 김민희 on 2021/07/18.
+//
+
+import UIKit
+
+class CheckRoutineVC: UIViewController {
+
+    
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
+// MARK: Extension
+
+extension CheckRoutineVC {
+    
+    
+}
