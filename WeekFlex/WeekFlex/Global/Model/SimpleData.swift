3d13a2b22f52c1553be27c9d94b0be2aa42c471d
jj-sh <adfilm2@naver.com>
2021-07-21 09:25:14 +0900

    [refac] SimpleData 구조체 수정

--- WeekFlex/WeekFlex/Global/Model/SimpleData.swift
+++ WeekFlex/WeekFlex/Global/Model/SimpleData.swift
@@ -9,13 +9,6 @@ import Foundation
 
 
 struct SimpleData: Codable {
-    let data: DataClass
-    let status: String
-    let statusCode: Int
-}
-
-// MARK: - DataClass
-struct DataClass: Codable {
-    
     let isSuccess: Bool
+    
 }
