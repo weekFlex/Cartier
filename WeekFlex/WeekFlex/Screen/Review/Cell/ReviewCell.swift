25a9ad202474b1c0a3b1f8324f1cf245e6864a37
dohan <adfilm2@naver.com>
2021-10-24 19:40:52 +0900

    [style] 회고 내용 없을때 마스코트/아이콘/텍스트 변경

--- WeekFlex/WeekFlex/Screen/Review/Cell/ReviewCell.swift
+++ WeekFlex/WeekFlex/Screen/Review/Cell/ReviewCell.swift
@@ -16,7 +16,11 @@ class ReviewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var reviewCharacter: UIImageView!
     @IBOutlet var categories: [UIImageView]!
+    @IBOutlet weak var warningIcon: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var detailButton: UIButton!
+    @IBOutlet weak var line: UIView!
+    @IBOutlet weak var warningConstraint: NSLayoutConstraint!
     
     
     override func awakeFromNib() {
@@ -40,29 +44,38 @@ class ReviewCell: UICollectionViewCell {
         dateLabel.text = "\(startDayString.changeDay()) ~ \(endDayString.changeDay())"
         titleLabel.text = data.title
         if(data.content != "" ){
+            warningIcon.isHidden = true
+            warningConstraint.constant = 5
             descriptionLabel.text = data.content
+            
         }else{
+            warningIcon.isHidden = false
+            warningConstraint.constant = 22
             descriptionLabel.text = "회고가 비어있어요"
-            let warningView = UIImageView()
-            let icon = UIImage(named: "warning.png")
-            warningView.image = icon
-            self.view.addSubview(warningView)
-            warningView.translatesAutoresizingMaskIntoConstraints = false
-            descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+//            let warningView = UIImageView()
+//            let icon = UIImage(named: "warning.png")
+//            warningView.image = icon
+//            self.view.addSubview(warningView)
+//            warningView.translatesAutoresizingMaskIntoConstraints = false
+//
+//
+//            warningView.widthAnchor.constraint(equalToConstant: 12).isActive = true
+//            warningView.heightAnchor.constraint(equalToConstant: 12).isActive = true
+//            warningView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 22).isActive = true
+//            warningView.topAnchor.constraint(equalTo: descriptionLabel.topAnchor).isActive = true
+//            warningView.trailingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor, constant: 6).isActive = true
+//
+//
+//            descriptionConstraint.secondItem = warningView
+//            descriptionConstraint.secondAttribute = trailingAnchor
+//            descriptionConstraint.isActive = true
+//            descriptionLabel.leadingAnchor.constraint(equalTo: warningView.trailingAnchor, constant: 6).isActive = true
             
-            warningView.widthAnchor.constraint(equalToConstant: 12).isActive = true
-            warningView.heightAnchor.constraint(equalToConstant: 12).isActive = true
-            warningView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 22).isActive = true
-            warningView.topAnchor.constraint(equalTo: descriptionLabel.topAnchor).isActive = true
-            warningView.trailingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor, constant: 6).isActive = true
-            
-            descriptionLabel.leadingAnchor.constraint(equalTo: warningView.trailingAnchor, constant: 6).isActive = true
-            descriptionLabel.trailingAnchor.constraint(equalTo: warningView.trailingAnchor, constant: 6).isActive = true
             
         }
         
         if let arr = myDelegate?.emotionMascot {
-            //
+            //0일경우 기본
             reviewCharacter.image = UIImage(named: "Character/character-96-\(arr[data.emotionMascot].0)")
         }
         if !data.stars.isEmpty {
