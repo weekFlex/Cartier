a709f81647563c3ae254aa6db785238a59a2a2a8
xwoud <xwoud@naver.com>
2022-05-05 02:25:20 +0900

    [feature] indexPath를 type으로 하는 변수를 새로 작성해서 조금 더 쉽게 color number에 접근

--- WeekFlex/WeekFlex/Screen/EditRoutine/ViewController/CreateCategoryVC.swift
+++ WeekFlex/WeekFlex/Screen/EditRoutine/ViewController/CreateCategoryVC.swift
@@ -8,43 +8,53 @@
 import UIKit
 
 class CreateCategoryVC: UIViewController {
-    
+
+    // MARK: Enum
     enum CategoryState {
         case making
         case editing(CategoryData)
     }
     
-    // MARK: - Variables
+    // MARK: Variables
     let didDismissCreateCategoryVC: Notification.Name = Notification.Name("DidDismissCreateCategoryVC")
     var hideViewDelegate: HideViewProtocol?
     var saveCategoryDelegate: SaveCategoryProtocol?
-    var checkedID: Int? {
-        didSet {
-            if let ID = checkedID {
-                setCategoryImage(num: ID)
-            }
-            categoryCollectionView.reloadData()
-            
-            if categoryTitle.text?.count != 0 || categoryTitle.text != nil {
-                completeButton.isEnabled = true
-            }
-            
-        }
-    }
     var checkedColor: Int?
     var state: CategoryState = .making
     private var categoryViewModel: CategoryViewModel?
     var dismissAction : (() -> Void)?
-    
-    // View Model
+    var checkedID: Int? {
+        didSet {
+            switch state {
+            case .making:
+                if let ID = checkedID {
+                    setCategoryImage(num: ID)
+                }
+                categoryCollectionView.reloadData()
+
+                checkEnableComplete()
+            case .editing(_):
+                break
+            }
+        }
+    }
+    var checkCategory: IndexPath? {
+        didSet {
+            categoryCollectionView.reloadData()
+            checkEnableComplete()
+        }
+    }
     private var categoryListViewModel : CategoryListViewModel?
     
     // alert
-    let alert = UIAlertController(title: "존재하는 카테고리", message: "이미 존재하는 카테고리 이름입니다.\n다른 이름을 입력해주세요.", preferredStyle: .alert)
-    let cancel = UIAlertAction(title: "확인", style: .cancel, handler : nil)
+    let alert = UIAlertController(title: "존재하는 카테고리",
+                                  message: "이미 존재하는 카테고리 이름입니다.\n다른 이름을 입력해주세요.",
+                                  preferredStyle: .alert)
+    let cancel = UIAlertAction(title: "확인",
+                               style: .cancel,
+                               handler : nil)
     
-    // MARK: IBOutlet
-    
+    // MARK: - @IBOutlet
     @IBOutlet var topConstraint: NSLayoutConstraint!
     @IBOutlet var modalBackgroundView: UIView!
     @IBOutlet var backButton: UIButton!
@@ -55,15 +65,16 @@ class CreateCategoryVC: UIViewController {
     @IBOutlet var categoryColorImage: UIImageView!
     @IBOutlet var categoryColorImageLength: NSLayoutConstraint!
     @IBOutlet var categoryCollectionView: UICollectionView!
-    
+
+    // MARK: @IBAction
     @IBAction func addCompleteButtonPressed(_ sender: Any) {
         
-        guard let colorID = checkedColor,
-              let categoryTitle = categoryTitle.text,
+        guard let categoryTitle = categoryTitle.text,
               let token = UserDefaults.standard.string(forKey: "UserToken") else { return }
         
         switch state {
         case .making:
+            guard let colorID = checkedColor else { return }
             APIService.shared.createCategory(token, color: colorID, name: categoryTitle) { result in
                 switch result {
 
@@ -79,7 +90,13 @@ class CreateCategoryVC: UIViewController {
         case .editing(_):
             guard let categoryViewModel = categoryViewModel,
                  let dismissAction = self.dismissAction else { return }
-            APIService.shared.updateCategory(token, color: colorID, id: categoryViewModel.ID, name: categoryTitle) { result in
+
+            let title = categoryTitle == categoryViewModel.title ? nil : categoryTitle
+
+            APIService.shared.updateCategory(token,
+                                             color: checkedColor ?? 0,
+                                             id: categoryViewModel.ID,
+                                             name: title) { result in
                 switch result {
                 case .success(_):
                     DispatchQueue.main.async {
@@ -93,14 +110,15 @@ class CreateCategoryVC: UIViewController {
         }
         
     }
-    @IBAction func cancleBtnDidTap(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func cancleBtnDidTap(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func textFieldStartEditingBtnTapped(_ sender: Any) {
-        self.categoryTitle.becomeFirstResponder()
+        categoryTitle.becomeFirstResponder()
     }
-    
+
+    // MARK: - Override Function
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
@@ -112,8 +130,7 @@ class CreateCategoryVC: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        // 이 뷰에 들어오자 마자 바로 키보드 띄우고 cursor 포커스 주기
-        self.categoryTitle.becomeFirstResponder()
+        categoryTitle.becomeFirstResponder()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -130,12 +147,17 @@ extension CreateCategoryVC {
         
         // background
         view.backgroundColor = UIColor(white: 0, alpha: 0.0)
-        modalBackgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(backgroundTapped)))
+        modalBackgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self,
+                                                                        action: #selector(backgroundTapped)))
         categoryColorView.backgroundColor = .white
+
         // header
         backButton.setImage(UIImage(named: "icon32CancleBlack"), for: .normal)
+        backButton.setTitle("", for: .normal)
         completeButton.setImage(UIImage(named: "icon32CheckBlack"), for: .normal)
+        completeButton.setTitle("", for: .normal)
         completeButton.isEnabled = false
+
         switch state {
         case .making:
             headerLabel.setLabel(text: "카테고리 추가", color: .black, font: .appleMedium(size: 18))
@@ -146,9 +168,14 @@ extension CreateCategoryVC {
             categoryTitle.text = categoryViewModel?.title
             categoryColorImage.image = UIImage(named: categoryViewModel?.categoryColorImageName ?? "icon-24-star-n")
             modalBackgroundView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-            checkedID = categoryViewModel?.ID
+            guard let color = categoryViewModel?.categoryColor else { return }
+            if color < 4 {
+                checkCategory = IndexPath(row: color-1, section: 0)
+            } else {
+                checkCategory = IndexPath(row: color-4, section: 1)
+            }
         }
-        topConstraint.constant = 40/896*self.view.bounds.height
+        topConstraint.constant = 40/896 * view.bounds.height
         
         // text field
         categoryTitle.borderStyle = .none
@@ -156,9 +183,11 @@ extension CreateCategoryVC {
         categoryTitle.font = .appleBold(size: 24)
         categoryTitle.adjustsFontSizeToFitWidth = true
         categoryTitle.minimumFontSize = 24
-        categoryTitle.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        categoryTitle.addTarget(self, action: #selector(self.textFieldDidChange(_:)),
+                                for: .editingChanged)
         categoryTitle.delegate = self
-        categoryColorImageLength.constant = 20/896*self.view.bounds.height
+        categoryColorImageLength.constant = 20/896 * self.view.bounds.height
+
         // collectionView
         categoryCollectionView.delegate = self
         categoryCollectionView.dataSource = self
@@ -167,18 +196,53 @@ extension CreateCategoryVC {
     func setCategoryImage(num: Int) {
         switch num < 10 {
         case true:
-            checkedColor = num+1
-            categoryColorImage.image = UIImage(named: "icon-24-star-n\(num+1)")
+            checkedColor = num + 1
+            categoryColorImage.image = UIImage(named: "icon-24-star-n\(num + 1)")
         case false:
-            checkedColor = num-6
-            categoryColorImage.image = UIImage(named: "icon-24-star-n\(num-6)")
+            checkedColor = num - 6
+            categoryColorImage.image = UIImage(named: "icon-24-star-n\(num - 6)")
         }
     }
 }
 
 // MARK: Method
-
 extension CreateCategoryVC {
+
+    func checkEnableComplete() {
+
+        guard let title = categoryTitle.text,
+                  title.count != 0 else {
+            completeButton.isEnabled = false
+            return
+        }
+
+        switch state {
+        case .making:
+            completeButton.isEnabled = (checkedID != nil) ? true : false
+
+        case .editing(_):
+            guard let checkCategory = checkCategory else {
+                completeButton.isEnabled = false
+                return
+            }
+
+            if checkCategory.section == 0 {
+                checkedColor = checkCategory.row + 1
+            } else {
+                checkedColor = checkCategory.row + 4
+            }
+
+            guard let categoryViewModel = categoryViewModel else {
+                return
+            }
+            if categoryViewModel.title == title &&
+                categoryViewModel.categoryColor == checkedColor {
+                completeButton.isEnabled = false
+            } else {
+                completeButton.isEnabled = true
+            }
+        }
+    }
     
     @objc func backgroundTapped(sender: UITapGestureRecognizer) {
         self.hideViewDelegate?.hideViewProtocol()
@@ -186,65 +250,65 @@ extension CreateCategoryVC {
     }
     
     @objc func textFieldDidChange(_ textField: UITextField) {
-        guard let text = textField.text else {
-            completeButton.isEnabled = false
-            return
-        }
-        print("\(#function) : \(text)")
-        if let checkedID = checkedID {
-            if text.count == 0 {
-                completeButton.isEnabled = false
-            } else {
-                completeButton.isEnabled = true
-            }
-        } else {
-            completeButton.isEnabled = false
-        }
+        checkEnableComplete()
     }
     
 }
 
 // MARK: UITextFieldDelegate
-
 extension CreateCategoryVC: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // 리턴 키 클릭 시
         textField.endEditing(true)
         return true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        // textField 클릭하면 무조건 키보드 올라오게
         textField.becomeFirstResponder()
     }
 }
 
+// MARK: UICollectionViewDelegateFlowLayout
 extension CreateCategoryVC: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt
-                            indexPath: IndexPath) -> CGSize {
-        let sideLength = 40/896*self.view.bounds.height
-        return CGSize(width: sideLength, height:sideLength)
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideLength = 40/896 * self.view.bounds.height
+        return CGSize(width: sideLength,
+                      height:sideLength)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 24, left: 20, bottom: 24, right: 20)
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 24,
+                            left: 20,
+                            bottom: 24,
+                            right: 20)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 22
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 24
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
         switch section {
         case 0:
-            return CGSize(width: 0, height: 0)
+            return CGSize(width: 0,
+                          height: 0)
         case 1:
-            return CGSize(width: view.bounds.width, height: 1)
+            return CGSize(width: view.bounds.width,
+                          height: 1)
         default:
             assert(false)
         }
@@ -252,12 +316,17 @@ extension CreateCategoryVC: UICollectionViewDelegateFlowLayout {
     
 }
 
+// MARK: UICollectionViewDataSource
 extension CreateCategoryVC: UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "reusableView", for: indexPath)
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                             withReuseIdentifier: "reusableView",
+                                                                             for: indexPath)
             headerView.backgroundColor = .gray1
             return headerView
         default:
@@ -282,48 +351,67 @@ extension CreateCategoryVC: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ColorCategoryCollectionViewCell.identifier, for: indexPath) as? ColorCategoryCollectionViewCell else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ColorCategoryCollectionViewCell.identifier,
+            for: indexPath) as? ColorCategoryCollectionViewCell else {
+            return UICollectionViewCell() }
         let firstSectionColorList: [UIColor] = [.color1, .color2, .color3]
-        let secondSectionColorList: [UIColor] = [.color04, .color05, .color06, .color07, .color08, .color09, .color10, .color11, .color12, .color13, .color14, .color15]
-        switch indexPath.section {
-        case 0:
-            cell.backgroundColor = firstSectionColorList[indexPath.row]
-        case 1:
-            cell.backgroundColor = secondSectionColorList[indexPath.row]
-        default:
-            assert(true)
-        }
+        let secondSectionColorList: [UIColor] = [.color04, .color05, .color06, .color07, .color08, .color09,
+                                                 .color10, .color11, .color12, .color13, .color14, .color15]
+
+        cell.backgroundColor = (indexPath.section == 0) ? firstSectionColorList[indexPath.row] : secondSectionColorList[indexPath.row]
         cell.setRounded(radius: nil)
-        
-        if let checkedID = checkedID {
+
+        switch state {
+
+        case .making:
+            guard let checkedID = checkedID else { return cell }
             if checkedID < 10 { // section 1
-                if  indexPath.section == 0 &&
-                        indexPath.row == checkedID {
+                if indexPath.section == 0 && indexPath.row == checkedID {
                     cell.checkImage.isHidden = false
                 } else {
                     cell.checkImage.isHidden = true
                 }
             } else { // section 2
-                if  indexPath.section == 1 &&
-                        indexPath.row == checkedID-10 {
+                if indexPath.section == 1 && indexPath.row == checkedID-10 {
                     cell.checkImage.isHidden = false
                 } else {
                     cell.checkImage.isHidden = true
                 }
             }
+        case .editing(_):
+            guard let checkCategory = checkCategory else { return cell }
+            cell.checkImage.isHidden = indexPath == checkCategory ? false : true
         }
+        
+
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        switch indexPath.section {
-        case 0:
-            checkedID = indexPath.row
-        case 1:
-            checkedID = indexPath.row+10
-        default:
-            return
+
+        switch state {
+        case .making:
+            switch indexPath.section {
+            case 0:
+                checkedID = indexPath.row
+            case 1:
+                checkedID = indexPath.row + 10
+            default:
+                return
+            }
+        case .editing(_):
+            checkCategory = indexPath
+            switch indexPath.section {
+            case 0:
+                categoryColorImage.image = UIImage(named: "icon-24-star-n\(indexPath.row+1)")
+            case 1:
+                categoryColorImage.image = UIImage(named: "icon-24-star-n\(indexPath.row+4 )")
+            default:
+                break
+            }
         }
+
     }
 }
