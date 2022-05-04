51aada39fb807e0d7eea8d2e27aba58f24f0442d
xwoud <xwoud@naver.com>
2022-05-04 12:09:02 +0900

    [upload] 카테고리 목록 Cell 생성

---
+++ WeekFlex/WeekFlex/Screen/EditRoutine/Cell/CategoryListCell.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryListCell.swift
+//  WeekFlex
+//
+//  Created by Hailey on 2022/05/04.
+//
+
+import UIKit
+
+class CategoryListCell: UITableViewCell {
+
+    static let identifier = "CategoryListCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configure(icon: String, title: String) {
+        
+    }
+
+}
