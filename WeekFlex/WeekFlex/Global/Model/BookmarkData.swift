e54c035effe76fe7beafe24a95a840622e3f923a
xwoud <xwoud@naver.com>
2022-10-19 18:40:08 +0900

    [upload] 새로운 파일 추가

---
+++ WeekFlex/WeekFlex/Global/Model/BookmarkData.swift
@@ -0,0 +1,8 @@
+//
+//  BookmarkData.swift
+//  WeekFlex
+//
+//  Created by Hailey on 2022/10/19.
+//
+
+import Foundation
---
+++ WeekFlex/WeekFlex/Global/Service/MoyaLoggingPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  MoyaLoggingPlugin.swift
+//  WeekFlex
+//
+//  Created by Hailey on 2022/10/19.
+//
+
+import Foundation
