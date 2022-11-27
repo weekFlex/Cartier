3290f756f9fb159a6f75510200013d11fef71fdc
xwoud <xwoud@naver.com>
2022-11-27 22:07:42 +0900

    [upload] mypage tableview로 바꾸기위해 cell 생성

---
+++ WeekFlex/WeekFlex/Screen/MyPage/Cell/MyPageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageCell.swift
+//  WeekFlex
+//
+//  Created by 미니 on 2022/11/27.
+//
+
+import UIKit
+
+class MyPageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
